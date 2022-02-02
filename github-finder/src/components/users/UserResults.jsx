083a923c3cc44e2f28a650797760445c65d49162
Alex Elmore <aelmore@merkleinc.com>
2022-02-02 12:43:31 -0600

    touchups to UserResults; removed github token

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -5,7 +5,7 @@ function UserResults() {
   const url = process.env.REACT_APP_GITHUB_URL;
   const token = process.env.REACT_APP_GITHUB_TOKEN;
 
-  // State for data that gets return back from GitHub and for loading Spinner
+  // State for data that gets return back from GitHub
   const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -16,24 +16,21 @@ function UserResults() {
 
   // function that makes a GET fetch request to gitHub
   const fetchGithubUsers = async () => {
-    const response = await fetch(`${url}/users`, {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    });
+    const response = await fetch(`${url}/users`);
     const data = await response.json();
     setUserData(data);
     setLoading(false);
   };
-  return loading ? (
-    <Spinner />
-  ) : (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {userData.map((user) => (
-        <h3 key={user.id}>{user.login}</h3>
-      ))}
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3">
+        {userData.map((user) => (
+          <h3 key={user.id}>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
-
 export default UserResults;
