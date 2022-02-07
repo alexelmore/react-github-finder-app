ca9f94af4f8b99e78eedfc3f1a8a1c7f2c709a46
Alex Elmore <aelmore@merkleinc.com>
2022-02-07 08:22:24 -0600

    repo item styling

--- github-finder/src/components/pages/About.jsx
+++ github-finder/src/components/pages/About.jsx
@@ -4,8 +4,9 @@ function About() {
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details. This
-        project was developed with and utilizes React JS (React Router, React's
-        Context API, React Hooks), Tailwind CSS and daisyUi.
+        project was developed with React JS (React Router, React's Context API,
+        React Hooks and React Custom Hooks), GitHub's API services, Tailwind CSS
+        and daisyUi.
       </p>
       <p className="text-lg text-gray-400">
         Version <span className="text-white">1.0.0</span>
